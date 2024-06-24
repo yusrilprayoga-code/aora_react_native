1ac6b15eca02e30b1dfac41743f6f59e2687f864
MOH. YUSRIL. PRAYOGA <124210006@STUDENT.UPNYK.AC.ID>
2024-06-24 14:39:12 +0700

    Update Structure Folder

--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">Aora</Text>
       <StatusBar style="auto" />
+      <Link href="/profile" style={{color : 'blue'}}>Go to Profile</Link>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
